320d68517ea2b0e67bf7e8961f9c64b1a0b3c84d
Ben Cull <bjcull@gmail.com>
2016-05-09 15:36:00 +0200

    Upgraded to RC1, added CORS for testing on-prem

--- src/SSW.TimePRO.VSTS/Startup.cs
+++ src/SSW.TimePRO.VSTS/Startup.cs
@@ -3,9 +3,10 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
 using Microsoft.AspNet.Builder;
+using Microsoft.AspNet.Hosting;
 using Microsoft.AspNet.Http;
 using Microsoft.AspNet.StaticFiles;
-using Microsoft.Framework.DependencyInjection;
+using Microsoft.Extensions.DependencyInjection;
 
 namespace SSW.TimePRO.VSTS
 {
@@ -14,6 +15,7 @@ namespace SSW.TimePRO.VSTS
         // For more information on how to configure your application, visit http://go.microsoft.com/fwlink/?LinkID=398940
         public void ConfigureServices(IServiceCollection services)
         {
+            services.AddCors();
         }
 
         public void Configure(IApplicationBuilder app)
@@ -21,7 +23,16 @@ namespace SSW.TimePRO.VSTS
             // Add the platform handler to the request pipeline.
             app.UseIISPlatformHandler();
 
+            app.UseCors(builder =>
+            {
+                builder.AllowAnyOrigin();
+                builder.AllowAnyHeader();
+                builder.AllowAnyMethod();
+            });
+
             app.UseStaticFiles();
         }
+
+        public static void Main(string[] args) => WebApplication.Run<Startup>(args);
     }
 }
